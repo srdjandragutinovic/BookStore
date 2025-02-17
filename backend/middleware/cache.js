734c16d412d55e6dc572afd7d4c203377a853af1
unknown <srdjandragutinovic1205>
2025-02-17 22:11:31 +0100

    last changes added README

--- backend/middleware/cache.js
+++ backend/middleware/cache.js
@@ -4,13 +4,11 @@ const cache = new Map();
 const cacheMiddleware = (req, res, next) => {
   const cacheKey = req.originalUrl;
 
-  // Check if the data is in the cache
   if (cache.has(cacheKey)) {
     console.log('Serving from cache');
     return res.json(cache.get(cacheKey));
   }
 
-  // If not in cache, proceed to the route handler
   next();
 };
 
--- backend/middleware/logger.js
+++ backend/middleware/logger.js
@@ -1,6 +1,6 @@
 // middleware/logger.js
 const morgan = require('morgan');
 
-const logger = morgan('combined'); // Logs in Apache combined format
+const logger = morgan('combined');
 
 module.exports = logger;
